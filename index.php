e96edbeb2242b6e72fef307093a4800e168ba366
subhendu <bhattasubhendu@gmail.com>
2019-12-15 16:15:15 +0545

    login form

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            background: grey;
+            padding: 5px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Welcome to FaceBook</h1>
+
+    <div>
+        <h5>Sigup here</h5>
+
+        <form action="">
+            <input type="text" placeholder="Enter Your name" name="name">
+            <br>
+            <input type="text" placeholder="Enter email" name="email">
+            <br>
+            <input type="password" placeholder="Enter Password" name="password">
+            <br>
+
+            <input type="submit" value="Register">
+
+
+        </form>
+
+    </div>
+</body>
+
+</html>
